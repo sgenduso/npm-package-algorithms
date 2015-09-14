133893553ceaa14869c82f586ea49ef3b2af1e02
Nick Mattei <nmattei89@gmail.com>
2015-09-14 09:30:59 -0600

    pangram

---
+++ pangram/spec/spec.js
@@ -0,0 +1,13 @@
+var myCode = require('../function.js');
+describe('isPangram', function() {
+  it('it checks if a string is a pangram', function() {
+    var pan1='Cwm fjord veg balks nth pyx quiz.'
+    var pan3='the brown fox'
+    var pan4='12abcdefghijklmno532pqrstuvwxYZ3'
+    var pan5='abcdefGHILMNOPqr s t u v'
+    expect(myCode.isPangram(pan1)).toEqual(true);
+    expect(myCode.isPangram(pan3)).toEqual(false);
+    expect(myCode.isPangram(pan4)).toEqual(true);
+    expect(myCode.isPangram(pan5)).toEqual(false);
+  });
+});
