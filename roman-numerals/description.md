836407fdde4cf3a71cae4ddac0f88c49d830409a
Amanda Carlton <acarlton27@gmail.com>
2015-09-16 20:10:51 -0600

    function to convert roman numerals to decimals

---
+++ roman-numerals/description.md
@@ -0,0 +1,6 @@
+This function returns the decimal value of a given Roman.  Factorials cannot be calculated on negative or fractional numbers like floats.  
+
+IT:
+  * Takes one argument: A whole, positive, integer
+  * Returns the product of the factorial
+  * Cannot accept Floats or Negative Numbers. Doing so causes stack overflow.
---
+++ roman-numerals/function.js
@@ -0,0 +1,27 @@
+
+var romans = {
+  'M': 1000,
+  'D': 500,
+  'C': 100,
+  'L': 50,
+  'X': 10,
+  'V': 5,
+  'I': 1
+};
+
+ function romanNumeral(incoming){
+    if(incoming.length === 0){
+      return 0;
+    }else{
+      if(romans[incoming[1]]>romans[incoming[0]]){
+      return (romans[incoming[1]] - romans[incoming[0]]) + romanNumeral(incoming.substring(2));
+    }else{
+      return romans[incoming[0]] + romanNumeral(incoming.substring(1));
+    }
+
+    }
+ }
+
+module.exports = {
+  romanNumeral: romanNumeral
+};
