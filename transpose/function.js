0b9e5061ad143b0daec222c095377fbea9dff386
Nick Mattei <nmattei89@gmail.com>
2015-09-21 20:45:47 -0600

    transpose/palindrome w/o spec

---
+++ transpose/function.js
@@ -0,0 +1,9 @@
+module.exports = {
+  transpose: function(){
+    return array = input.map(function(e1,i){
+      return temp = input.map(function(e2,j){
+        return input[j][i]
+      })
+    });
+  }
+};
