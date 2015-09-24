fb16b19c29530875bc0ad859a102669b52946c92
Sam Genduso <sam.genduso@gmail.com>
2015-09-23 20:25:35 -0600

    merge?

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
 
-gulp.task('default', ['UnitTests','watchout'])
+gulp.task('default', ['UnitTests','watchout']);
 
 gulp.task('UnitTests', function () {
     return gulp.src('tests/spec/*spec.js')
@@ -12,5 +12,5 @@ gulp.task('ci', ['default']);
 
 
 gulp.task('watchout', function () {
-  gulp.watch('tests/spec/*spec.js', ['UnitTests'])
-})
+  gulp.watch('tests/spec/*spec.js', ['UnitTests']);
+});
