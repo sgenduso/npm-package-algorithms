5433588c7a688b8faba50bb4ab4080689c9bb516
Nick Mattei <nmattei89@gmail.com>
2015-09-24 09:00:59 -0600

    add spec

--- transpose/function.js
+++ transpose/function.js
@@ -1,9 +1,13 @@
-module.exports = {
-  transpose: function(){
+
+  var transpose = function(input){
     return array = input.map(function(e1,i){
       return temp = input.map(function(e2,j){
         return input[j][i]
       })
     });
   }
-};
+
+
+  module.exports = {
+    transpose: transpose
+  };
