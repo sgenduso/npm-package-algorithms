55abd73f19c5ef2947bedd2a0098e1d0ff6c3f70
Sam Genduso <sam.genduso@gmail.com>
2015-09-23 20:36:24 -0600

    findCount, search, fuzzyMatch with passing tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
 
-gulp.task('default', ['UnitTests','watchout']);
+gulp.task('default', ['UnitTests']);
 
 gulp.task('UnitTests', function () {
     return gulp.src('tests/spec/*spec.js')
@@ -10,6 +10,17 @@ gulp.task('UnitTests', function () {
 
 gulp.task('ci', ['default']);
 
-gulp.task('watchout', function () {
-  gulp.watch('tests/spec/*spec.js', ['UnitTests']);
-});
+
+
+// gulp.task('default', ['UnitTests','watchout']);
+//
+// gulp.task('UnitTests', function () {
+//     return gulp.src('tests/spec/*spec.js')
+//         .pipe(jasmine());
+// });
+//
+// gulp.task('ci', ['default']);
+//
+// gulp.task('watchout', function () {
+//   gulp.watch('tests/spec/*spec.js', ['UnitTests']);
+// });
